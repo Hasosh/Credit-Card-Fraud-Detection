a90aa1ac901202f7645d72ca8d38827e4892343c
TareqKomboz <Tareq.Komboz@hotmail.com>
2023-12-06 18:38:05 +0100

    Added empty KMeans file

---
+++ src/KMeans.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import pickle as pkl
+
+if __name__ == '__main__':
+    # Data loading
+    df_head = pd.read_csv('../data/creditcard_2023_head.csv')
+
+    # Loading first setup
+    with open('../data/setup_1.pkl', 'rb') as f:
+        setup1 = pkl.load(f)
+
+    X_train, _, X_test, y_test = setup1['X_train'], setup1['y_train'], setup1['X_test'], setup1['y_test']
+
+    # Assuming the first column is 'id' and the last column is 'amount'
+    columns = ['Feature_' + str(i) for i in range(1, X_train.shape[1] - 1)] + ['Amount']
+    X_train_df = pd.DataFrame(X_train[:, 1:], columns=columns)  # Excluding 'id'
